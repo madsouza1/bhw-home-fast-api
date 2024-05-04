7d82f5e27e36a292ebb5d288fa014f9ab3fe13a6
git_repo_user <evalgit@example.com>
2024-05-04 14:25:47 -0400

    hello now

--- startup.sh
+++ startup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Change directory to the fastapi directory
-cd fastapi
-
-# Activate any virtual environment if necessary
-# source /path/to/your/venv/bin/activate
-
 # Run your FastAPI application using Gunicorn with Uvicorn workers
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+uvicorn main:app --app-dir  ./fastapi
